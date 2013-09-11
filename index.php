3cafe63c374494481fdc9d3630b274a1e028e9ce
hnlatham <hnlatham@gmail.com>
2013-09-11 18:56:23 -0400

    Adding first index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title></title>
+	
+	<link rel="stylesheet" href="css/latham.css" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Nunito' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<div class="container">
+		<header>
+			<h1>Hello, world</h1>
+			<p>Welcome to my playground.</p>
+		</header>
+	</div>
+
+</body>
+</html>
